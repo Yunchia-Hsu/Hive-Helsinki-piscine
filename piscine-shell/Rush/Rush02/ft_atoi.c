bdabeb406781016bf3b8be1679c8944006f0b7eb
Yunchia <127295359+Yunchia-Hsu@users.noreply.github.com>
2023-07-17 15:56:21 +0300

    Create ft_atoi.c

---
+++ piscine-shell/Rush/Rush02/ft_atoi.c
@@ -0,0 +1,28 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	j;
+	int	x;
+
+	i = 0;
+	x = 0;
+	j = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+	{
+		i++;
+	}
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			x++;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		j = j * 10 + str[i] - 48;
+		i++;
+	}
+	if (x % 2 == 1)
+		j = j * -1;
+	return (j);
+}
