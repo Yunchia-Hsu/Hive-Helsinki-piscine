181b0322c2032403b29c3ea16ccf67f0c894e98c
Yunchia <127295359+Yunchia-Hsu@users.noreply.github.com>
2023-07-17 15:54:49 +0300

    Create main.c

---
+++ piscine-shell/Rush/Rush02/main.c
@@ -0,0 +1,33 @@
+#include<unistd.h>
+
+int	fill(int val);
+int	ft_atoi(char *str);
+
+int	valid_a(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < 48 || str[i] > 57)
+			return (0);
+		i++;
+	}
+	if (i > 37)
+		return (0);
+	return (1);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc < 2 || argc > 3)
+		write(1, "Error\n", 6);
+	if (argc == 2)
+	{
+		if (valid_a(argv[1]) == 0)
+			write(1, "Error\n", 6);
+	}
+	fill(ft_atoi(argv[1]));
+	return (1);
+}
